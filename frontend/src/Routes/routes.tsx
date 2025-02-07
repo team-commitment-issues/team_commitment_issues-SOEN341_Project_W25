5abea8b8c135634463573738a6899993eda9c4e7
MohamadAliTaha <mohamadalitaha03@gmail.com>
2025-02-07 16:04:02 -0500

    added a new lading page for the user to decide whether to go to login or signup

--- frontend/src/Routes/routes.tsx
+++ frontend/src/Routes/routes.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 // Lazy-loaded components
 const Login = lazy(() => import('../Pages/Login'));
 const SignUp = lazy(() => import('../Pages/SignUp'));
+const LandingPage = lazy(() => import('../Pages/LandingPage'));
 
 const AppRoutes: React.FC = () => {
   return (
@@ -11,8 +12,8 @@ const AppRoutes: React.FC = () => {
       <Routes>
         <Route path="/login" element={React.createElement(Login)} />
         <Route path="/signup" element={React.createElement(SignUp)} />
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="/" element={React.createElement(LandingPage)} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Suspense>
   );
