bdb026331ab8304845c1dfb1facebfeb94bb578c
MohamadAliTaha <mohamadalitaha03@gmail.com>
2025-02-13 20:25:14 -0500

    fixed the backend requests for creating a new team by the super admin

--- backend/src/services/superAdminService.ts
+++ backend/src/services/superAdminService.ts
@@ -3,11 +3,33 @@ import Channel from '../models/Channel';
 import User from '../models/User';
 import Team from '../models/Team';
 import TeamMember from '../models/TeamMember';
+import { ObjectId } from 'mongoose';
 
 class SuperAdminService {
     static async createTeam(name: string, createdByUserID: string): Promise<any> {
-        /* TODO: Implement this method */
-        return null;
+        const team = new Team({
+            name,
+            createdBy: createdByUserID,
+        });
+
+        await team.save();
+
+        const teamMember = new TeamMember({
+            user: createdByUserID,
+            team: team._id,
+            role: TeamRole.ADMIN,
+        });
+
+        await teamMember.save();
+
+        const user = await User.findById(createdByUserID);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        user.teamMemberships.push(teamMember._id as ObjectId);
+        await user.save();
+
+        return team;
     }
 
     static async addUserToTeam(userID: string, teamID: string, teamRole: TeamRole): Promise<any> {
