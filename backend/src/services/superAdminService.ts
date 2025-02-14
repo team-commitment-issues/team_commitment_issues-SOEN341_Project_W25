ab64128897bbf8138d86c9d66cae39ade9012aa8
MohamadAliTaha <mohamadalitaha03@gmail.com>
2025-02-13 20:54:37 -0500

    added error catching and created the addUserToTeam service function

--- backend/src/services/superAdminService.ts
+++ backend/src/services/superAdminService.ts
@@ -7,34 +7,43 @@ import { ObjectId } from 'mongoose';
 
 class SuperAdminService {
     static async createTeam(name: string, createdByUserID: string): Promise<any> {
+        const possibleTeam = await Team.findOne({ name });
+        if (possibleTeam) {
+            throw new Error('Team already exists');
+        }
+
         const team = new Team({
             name,
             createdBy: createdByUserID,
         });
 
         await team.save();
-
-        const teamMember = new TeamMember({
-            user: createdByUserID,
-            team: team._id,
-            role: TeamRole.ADMIN,
-        });
-
-        await teamMember.save();
-
-        const user = await User.findById(createdByUserID);
-        if (!user) {
-            throw new Error('User not found');
-        }
-        user.teamMemberships.push(teamMember._id as ObjectId);
-        await user.save();
+        await SuperAdminService.addUserToTeam(createdByUserID, team._id as string, TeamRole.ADMIN);
 
         return team;
     }
 
-    static async addUserToTeam(userID: string, teamID: string, teamRole: TeamRole): Promise<any> {
-        /* TODO: Implement this method */
-        return null;
+    static async addUserToTeam(userID: string, teamID: string, role: TeamRole): Promise<any> {
+        const team = await Team.findById(teamID);
+        if (!team) {
+            throw new Error('Team not found');
+        }
+        const user = await User.findById(userID);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        
+        const teamMember = new TeamMember({
+            user: userID,
+            team: teamID,
+            role: role,
+        });
+
+        await teamMember.save();
+
+        user.teamMemberships.push(teamMember._id as ObjectId);
+
+        return teamMember;
     }
 }
 
