e1c506f612c6f0c11f47d88392a723b18d501a3f
Shamsam <60717097+Shamsam@users.noreply.github.com>
2025-03-16 22:10:41 -0400

    Enhance DirectMessageService with detailed JSDoc comments for createDirectMessage, getDirectMessages, and sendDirectMessage methods

--- ChatHavenApp/backend/src/services/directMessageService.ts
+++ ChatHavenApp/backend/src/services/directMessageService.ts
@@ -4,10 +4,26 @@ import TeamMember from '../models/TeamMember';
 import { Schema, Types } from 'mongoose';
 import DMessage from '../models/DMessage';
 import { Role } from '../enums';
-import DirectMessage from '../models/DirectMessage';
+import DirectMessage, { IDirectMessage } from '../models/DirectMessage';
 
 class DirectMessageService {
-    static async createDirectMessage(username: string, sender: Schema.Types.ObjectId, team: Schema.Types.ObjectId) {       
+    /**
+     * #### Creates a direct message between the sender and the receiver identified by the username.
+     * 
+     * @param username - The username of the receiver.
+     * @param sender - The ObjectId of the sender.
+     * @param team - The ObjectId of the team.
+     * @returns `Promise<DirectMessage>` The created direct message. See {@link DirectMessage}.
+     * @remarks If the receiver is not a SUPER_ADMIN, the receiver must be a team member of the team.
+     * 
+     * ***
+     * #### Exceptions
+     * @throws `Invalid username` when the username is not a string.
+     * @throws `User not found` when the receiver is not found.
+     * @throws `Team member not found` when the receiver is not a team member.
+     * @throws `Direct message already exists` when the direct message already exists.
+     */
+    static async createDirectMessage(username: string, sender: Schema.Types.ObjectId, team: Schema.Types.ObjectId)  {       
         if (typeof username !== 'string') {
                     throw new Error('Invalid username');
                 }
@@ -30,10 +46,17 @@ class DirectMessageService {
                     dmessages: [],
                 });
                 await directMessage.save();
-                console.log(directMessage);
                 return directMessage;
     }
 
+    /**
+     * #### Retrieves direct messages associated with a given direct message ID.
+     *
+     * @param directMessageId - The ID of the direct message to retrieve.
+     * @returns `Promise<DMessage[]>` A promise that resolves to an array of direct messages. See {@link DMessage}.
+     * ***
+     * @throws `Direct message not found` when the direct message is not found.
+     */
     static async getDirectMessages(directMessageId: Types.ObjectId) {
         const directMessage = await DirectMessage.findById(directMessageId);
         if (!directMessage) {
@@ -43,6 +66,16 @@ class DirectMessageService {
         return dmessages;
     }
 
+    /**
+     * #### Sends a direct message to a user in a direct message.
+     * 
+     * @param text - The text of the direct message.
+     * @param username - The username of the sender.
+     * @param directMessageId - The ID of the direct message.
+     * @returns `Promise<DMessage>` The created direct message. See {@link DMessage}.
+     * ***
+     * @throws `Direct message not found` when the direct message is not found.
+     */
     static async sendDirectMessage(text: string, username: string, directMessageId: Types.ObjectId) {
         const directMessage = await DirectMessage.findById(directMessageId);
         if (!directMessage) {
